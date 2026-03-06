11f4878025e76596bfb92587ee1285309796beb6
Maria Pol <maria.pujolpol@gmail.com>
2026-03-06 17:02:31 +0100

    User routes improvement

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,76 +2,11 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const verifyToken = require("../middlewares/auth.middlewares")
 
-// POST /api/users - Create user dev test only
-router.post("/users", async (req, res, next) => {
-  try {
-    const createdUser = await User.create(req.body);
-    console.log("BODY:", req.body);
-    res.status(201).json(createdUser);
-  } catch (error) {
-    next(error);
-  }
-});
 
-// GET /api/users - Devuelve todos los usuarios (solo para desarrollo) // dev test only
-
-
-router.get("/users", async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// GET /api/users/:id - Devuelve un usuario por ID // dev test only
-
-/*router.get("/users/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    const user = await User.findById(id).select("-password");
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(user);
-
-  } catch (error) {
-    next(error);
-  }
-});*/
-
-// PUT /api/users/:id - Actualiza un usuario por ID // dev test only
-
-/*router.put("/users/:id", async (req, res, next) => {
-  try {
-    const updated = await User.findByIdAndUpdate(req.params.id, req.body);
-
-    res.status(200).json(updated);
-  } catch (error) {
-    console.log('error');
-    next(error);
-  }
-});*/
 
 
 
 // PRIVATE ROUTES - require token verification
-
-// GET /api/users/:id - Devuelve el perfil del usuario autenticado
-router.get("/users/:id", verifyToken, async (req, res, next) => {
-  try {
-    const userId = req.payload._id;
-    const user = await User.findById(userId)
-    .select("-password -email");
-    res.json(user);
-  } catch (error) {
-    next(error);
-  }
-});
 
 
 // GET /api/users/me - get my profile
@@ -90,10 +25,23 @@ router.get("/users/me", verifyToken, async (req, res, next) => {
   }
 });
 
+// GET /api/users/:id - devuelve el perfil de otro usuario
+router.get("/users/:id", verifyToken, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id).select("-password -email");
+
+    res.status(200).json(user);
+ } catch (error) {
+    next(error);
+ }
+});
 
 
 
-// PUT /api/users/:id - Actualiza un usuario por ID // dev test only
+
+// PUT /api/users/:id - Actualiza un usuario por ID // 
 
 router.put("/users/:id", verifyToken, async (req, res, next) => {
   try {
